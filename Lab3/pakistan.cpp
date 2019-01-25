e02fc48f63d6fb5cd3099ccabca5735ea24f3682
sharanyakamath <sherrykamath@gmail.com>
2019-01-25 11:14:24 +0530

    changes

--- Lab3/pakistan.cpp
+++ Lab3/pakistan.cpp
@@ -13,8 +13,6 @@ void disp(){
         	glVertex2f(-0.4,-0.3);
         	
 	glEnd();
-	star(0.05,0.2,0.1);
-	//moon(0.1,-0.1,0.5);
 	glBegin(GL_POLYGON);
         	glColor3ub(139,69,19);
         	glVertex2f(-0.4,0.3);
@@ -22,6 +20,27 @@ void disp(){
         	glVertex2f(-0.38,-0.6);
         	glVertex2f(-0.38,0.3);
         	
+	glEnd();
+	
+	glBegin(GL_TRIANGLES);
+        	glColor3ub(255,255,255);
+        	glVertex2f(0.1,0.2);
+        	glVertex2f(0.15,0.2);
+        	glVertex2f(0.125,0.15);	
+	glEnd();
+	
+	glBegin(GL_TRIANGLES);
+        	glColor3ub(255,255,255);
+        	glVertex2f(0.1,0.152);
+        	glVertex2f(0.15,0.152);
+        	glVertex2f(0.125,0.22);	
+	glEnd();
+	
+	glBegin(GL_TRIANGLES);
+        	glColor3ub(139,69,19);
+        	glVertex2f(0.125,0.152);
+        	glVertex2f(0.15,0.15);
+        	glVertex2f(0.1,0.15);	
 	glEnd();
 	
 	const float DEG2RAD = 3.14159/180;
@@ -45,64 +64,6 @@ void disp(){
 	glEnd();
 	
 	glFlush();
-}
-void star(double d,double rx,double ry)
-{
-    const double t=0.38196;
-    const double transform=3.1415926/180;
-    //get the star vertex position compared with center
-    GLdouble draw_star[10][2]={
-                                0,1,//A 1
-                                cos( 54*transform )*t,t*sin( 54*transform ),//dd 2
-                                cos( 18*transform  ),sin( 18*transform  ),//b 3
-                                cos( 18*transform  )*t,-t*sin( 18*transform  ),//ee 4
-                                cos( 54*transform ),-sin( 54*transform ),//c 5
-                                0,-t,//aa 6
-                                -cos( 54*transform ),-sin( 54*transform ),//d 7
-                                -t*cos( 18*transform  ),-t*sin( 18*transform  ),//bb 8
-                                -cos( 18*transform  ),sin( 18*transform  ),//E 9
-                                -t*cos( 54*transform ),t*sin( 54*transform ),//cc 10
-
-    };
-    //get the size of star
-    const GLdouble size=d;
-    //draw first triangle
-    glColor3f(1,1,1);
-    glBegin(GL_POLYGON);//the star
-    glVertex2d(draw_star[0][0]*size+rx,draw_star[0][1]*size+ry);
-    glVertex2d(draw_star[4][0]*size+rx,draw_star[4][1]*size+ry);
-    glVertex2d(draw_star[7][0]*size+rx,draw_star[7][1]*size+ry);
-    glEnd();
-    //draw second triangle
-    glBegin(GL_POLYGON);//the star
-    glVertex2d(draw_star[2][0]*size+rx,draw_star[2][1]*size+ry);
-    glVertex2d(draw_star[5][0]*size+rx,draw_star[5][1]*size+ry);
-    glVertex2d(draw_star[8][0]*size+rx,draw_star[8][1]*size+ry);
-    glEnd();
-    //draw third small triangle
-    glBegin(GL_POLYGON);//the star
-    glVertex2d(draw_star[5][0]*size+rx,draw_star[5][1]*size+ry);
-    glVertex2d(draw_star[6][0]*size+rx,draw_star[6][1]*size+ry);
-    glVertex2d(draw_star[7][0]*size+rx,draw_star[7][1]*size+ry);
-    glEnd();
-    glFlush();
-
-}
-
-void moon(float step, float radius, float cut) {
-	float angle=0.0f;
-	float PI=3.14;
-	glBegin(GL_POLYGON);
-	glColor4f(1.0, 1.0, 1.0, 1.0);
-	glVertex2f(cut*radius*sinf(-1.5f*PI),0);
-	while (angle<=PI) {
-		glVertex2f(radius*sinf(angle),radius*cosf(angle));
-		angle+=step;
-	}
-	while (angle<(2.0f*PI)) {
-		angle+=step;
-	}
-	glEnd();
 }
 
 int main(int argc, char** argv){
